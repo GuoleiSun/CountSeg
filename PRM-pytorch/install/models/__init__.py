de1fa0b022d2df06e5056c004f73b8c931e642ec
GuoleiSun <guolei.sun@kaust.edu.sa>
2019-05-09 11:26:24 +0400

    Add files via upload

--- PRM-pytorch/install/models/__init__.py
+++ PRM-pytorch/install/models/__init__.py
@@ -1,6 +1,4 @@
 from .fc_resnet import FC_ResNet
-from .fc_resnet import FC_VGG16
-from .fc_resnet import FC_VGG16_2
 
 
-__all__ =['FC_ResNet','FC_VGG16','FC_VGG16_2']
+__all__ =['FC_ResNet']
--- PRM-pytorch/install/models/fc_resnet.py
+++ PRM-pytorch/install/models/fc_resnet.py
@@ -5,7 +5,7 @@ from torchvision import models
 
 class FC_ResNet(nn.Module):
 
-    def __init__(self, model, num_classes, selu):
+    def __init__(self, model, num_classes):
         super(FC_ResNet, self).__init__()
 
         # feature encoding
@@ -21,62 +21,10 @@ class FC_ResNet(nn.Module):
 
         # classifier
         num_features = model.layer4[1].conv1.in_channels
-        print('selu: ',selu)
-        if selu:
-            self.classifier = nn.Sequential(
-                nn.Conv2d(num_features, num_classes, kernel_size=1, bias=True), nn.SELU())
-        else:
-            self.classifier = nn.Sequential(
-                nn.Conv2d(num_features, num_classes, kernel_size=1, bias=True))
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
-        #print('relu')
-        return x
-
-
-class FC_VGG16(nn.Module):
-
-    def __init__(self, model, num_classes):
-        super(FC_VGG16, self).__init__()
-
-        # feature encoding
-        # self.features = nn.Sequential(
-        #     model.features)
-        self.features=nn.Sequential(*list(model.features.children())[:-1])
-        # classifier
-        num_features = 512
-
         self.classifier = nn.Sequential(
             nn.Conv2d(num_features, num_classes, kernel_size=1, bias=True))
- 
 
     def forward(self, x):
         x = self.features(x)
         x = self.classifier(x)
-        #print('relu')
-        return x
-
-
-class FC_VGG16_2(nn.Module):
-
-    def __init__(self, model, num_classes):
-        super(FC_VGG16_2, self).__init__()
-
-        # feature encoding
-        # self.features = nn.Sequential(
-        #     model.features)
-        self.features=nn.Sequential(*list(model.features.children())[:])
-        # classifier
-        num_features = 512
-
-        self.classifier = nn.Sequential(
-            nn.Conv2d(num_features, num_classes, kernel_size=1, bias=True))
- 
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
-        #print('relu')
         return x

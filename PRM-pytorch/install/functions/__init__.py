0f69ca537351ac6fbf5fecd147a2ade7b7538d00
GuoleiSun <guolei.sun@kaust.edu.sa>
2019-05-09 11:25:50 +0400

    Add files via upload

--- PRM-pytorch/install/functions/__init__.py
+++ PRM-pytorch/install/functions/__init__.py
@@ -1,13 +1,5 @@
 from .peak_backprop import pr_conv2d
 from .peak_stimulation import peak_stimulation_ori
-from .peak_stimulation import peak_stimulation_rel
-from .peak_stimulation import peak_stimulation_rel2
-from .peak_stimulation import peak_stimulation_rel3
-from .peak_stimulation import peak_stimulation_rel3_norelu
-from .peak_stimulation import peak_stimulation_ncrelu
-from .peak_stimulation import peak_stimulation_class_reg
-from .peak_stimulation import peak_stimulation_sort_boost
 from .peak_stimulation import peak_stimulation_ori_gt
-from .peak_stimulation import peak_stimulation_ori_gt_selected_flow_graph
 
-__all__ = ['pr_conv2d', 'peak_stimulation_ori','peak_stimulation_rel','peak_stimulation_rel2','peak_stimulation_rel3','peak_stimulation_rel3_norelu','peak_stimulation_ncrelu','peak_stimulation_class_reg','peak_stimulation_sort_boost','peak_stimulation_ori_gt','peak_stimulation_ori_gt_selected_flow_graph']
+__all__ = ['pr_conv2d', 'peak_stimulation_ori','peak_stimulation_ori_gt']
